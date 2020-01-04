b07fc18d776c70d271f6e84ce2dc231cea5c459f
Aurangzaib <aurangzaib.danial@gmail.com>
2020-01-04 18:58:02 +0500

    lists all todo_lists on root. New lists can now be created and a specific can be retrieved and shown

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,4 +1,29 @@
 class ListsController < ApplicationController
+  before_action :set_list, only: :show
+
   def index
+    @list = List.new
+    @lists = List.all
+  end
+
+  def create
+    @list = List.new(list_params)
+    @list.save
+
+    redirect_to @list
+  end
+
+  def show
+
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:title)
+  end
+
+  def set_list
+    @list = List.find(params[:id])
   end
 end
