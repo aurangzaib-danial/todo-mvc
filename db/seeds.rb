4341f09f4f318e7927bfc0b88d85321fdacfadf7
Aurangzaib <aurangzaib.danial@gmail.com>
2020-04-15 01:42:08 +0500

    write some seed data for development

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+users = [
+  {email: 'sunny@khan.com', password: 'khan1234'},
+  {email: 'avi@fs.com', password: 'avi1234'},
+  {email: 'jay@dota.com', password: 'jay1234'},
+]
+
+User.create!(users)
+
+sunny = User.first
+avi = User.second
+jay = User.third
+
+list_1 = sunny.personal_lists.create!(title: 'Learning Rails')
+list_1.collaborator_lists.create!(collaborator: avi)
+
+list_2 = avi.personal_lists.create!(title: 'Things to teach')
+list_2.collaborator_lists.create!([{collaborator: sunny}, {collaborator: jay}])
+
+list_3 = jay.personal_lists.create!(title: 'Learning Dota')
+list_3.collaborator_lists.create!({collaborator: sunny})
